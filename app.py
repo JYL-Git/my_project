561cd284d9ce74e6f9b3f90092d30ac54551e75e
Jae Yong Lee <jaeyong234@hotmail.com>
2021-02-27 18:54:42 +0900

    Home page draft

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 from pymongo import MongoClient
 
 from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index02.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
