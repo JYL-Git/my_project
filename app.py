8f1dcf17dc7ca451031dc172a61d471249aada78
Jae Yong Lee <jaeyong234@hotmail.com>
2021-03-05 22:51:44 +0900

    link two html

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/seoul_art_center')
+def seoul_art_center():
+    return render_template('seoul_art_center.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
