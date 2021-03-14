26b1e4cc9b0c83fce7cf81d1c76754aed994b6c5
Jae Yong Lee <jaeyong234@hotmail.com>
2021-03-14 23:55:13 +0900

    final

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ def seoul_art_center():
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     data = requests.get('https://www.sac.or.kr/site/main/program/schedule#n', headers=headers)
     soup = BeautifulSoup(data.text, 'html.parser')
-    # logo = soup.select('#header > div.width-auto > div > div.logo > a > img')
     exhibitions = soup.select('.romantic-slide > div')
 
     # Extract links and append to the list Links[]
@@ -38,7 +37,6 @@ def seoul_art_center():
         else:
             break
 
-
     # Inside each link, get title, image, and info (description)
     exhibition_list = []
     for link in links:
@@ -48,22 +46,35 @@ def seoul_art_center():
         url_title = og_title['content']
         og_image = soup.select_one('meta[property="og:image"]')
         url_image = og_image['content'].split(".jpg")[0]
-        # title = soup.select_one('#contents > div.cwa-top.show-view > dl > dt > p.title > span').text
         duration = soup.select_one('#contents > div.cwa-top.show-view > dl > dt > ul > li:nth-child(1) > span:nth-child(2)').text.strip()
-
-    #     temp = [title, duration]
-    #     # info = soup.select_one('#contents > div.cwa-top.show-view > dl > dt')
-    #     # src_exhibition = {'title': title}
-    #     # db.exhibition.insert_one(src_exhibition)
         temp = {'title': url_title, 'img': url_image, 'duration': duration}
-        # if db.exhibition.find_one(temp):
-        #     pass
-        # else:
-        #     db.exhibition.insert_one(temp)
         exhibition_list.append(temp)
-    # result = list(db.exhibition.find({}, {'_id': 0}))
 
     return jsonify({'result': 'success', 'exhibition': exhibition_list})
+
+@app.route('/open_arte_museum')
+def open_arte_museum():
+    return render_template('arte_museum.html')
+
+@app.route('/arte_museum', methods=['GET'])
+def arte_museum():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get('http://artemuseum.com/', headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+    exhibitions = soup.select('#s2020090711366db0dfcc7 > main > div:nth-child(4) > div > div > div > div > div > div')
+
+    exhibition_list = []
+    for exhibition in exhibitions:
+        title = exhibition.select_one('#container_w2020090801b6efdb353b6 > div > div > h4').text.strip()
+        image = exhibition.select_one('#gal_item_ > img')['data-original']
+
+        temp = {'title': title, 'image': image}
+
+        exhibition_list.append(temp)
+
+    return jsonify({'result': 'success', 'exhibition': exhibition_list})
+#gal_item_ > img
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
